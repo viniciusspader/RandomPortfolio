a3c804bece4bdb084b9e256ca4918e2ac6f3b1b4
Vinicius Spader <spader.vinicius@gmail.com>
2013-07-15 17:19:19 -0300

    Creating PortfolioController

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,4 +16,32 @@ function SettingsController($scope) {
   $scope.addSetting = function () {
     $scope.settings.push(new setting($scope.settings.qtAtivos, $scope.settings.qtCarteiras));
   };
+
+  $scope.getSettings = function () {
+    return $scope.settings;
+  };
+};
+
+function PortfolioController($scope) {
+  $scope.getSettings = function () {
+    return SettingsController.getSettings();
+  };
+
+  $scope.settings = $scope.getSettings();
+
+  $scope.portfolio = [];
+
+  function asset() {
+    return Math.floor(Math.random()*61)
+  }
+
+  function createAssetGroup(group_length) {
+    var group = [];
+    if (group.length < group_length) {
+      group.push(new asset());
+      createAssetGroup(group_length);
+    }
+    return group;
+  };
+
 };
