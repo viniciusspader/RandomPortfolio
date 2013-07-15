b6be073140baf37a2a706aa89f3c6c4989a9d942
Vinicius Spader <spader.vinicius@gmail.com>
2013-07-15 16:44:28 -0300

    Removing settings

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,19 @@
+'use strict'
+
 function SettingsController($scope) {
   $scope.settings = [];
 
   function setting(ativos, carteiras) {
+    //this.index = $scope.settings.length;
     this.qtAtivos = ativos;
     this.qtCarteiras = carteiras;
-  }
+  };
 
-  $scope.addSetting = function() {
+  $scope.removeSetting = function (setting) {
+    $scope.settings.splice($scope.settings.indexOf(setting), 1);
+  };
+
+  $scope.addSetting = function () {
     $scope.settings.push(new setting($scope.settings.qtAtivos, $scope.settings.qtCarteiras));
-  }
-}
+  };
+};
