008a4929dd027a7fa67362e24830dd241579053e
Vinicius Spader <spader.vinicius@gmail.com>
2013-07-22 10:19:04 -0300

    Adding node-static

--- scripts/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer( function (req, res) {
-	res.writeHead(200, {'Content-type':'text/plain'});
-	res.end('index.html');
-}).listen(8000);
-console.log('Server running at localhost:8000')
